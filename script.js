363509855f1b1f6c39ca29fccca1ae6600f587e8
Rezmii <bartoszrezmer20@gmail.com>
2024-03-12 11:49:49 +0100

    added dialog with adding books function

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const readInput = document.querySelector("#read");
-const addButton = document.querySelector(".add-button");
+const libraryDiv = document.querySelector(".library-div");
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+const dialogAddButton = document.querySelector("dialog .add-button");
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+displayBooks(myLibrary);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -12,17 +27,37 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-addButton.addEventListener("click", () => {
-  addBookToLibrary(
-    titleInput.value,
-    authorInput.value,
-    pagesInput.value,
-    readInput.value
-  );
+dialogAddButton.addEventListener("click", () => {
+  if (
+    titleInput.value !== "" &&
+    authorInput.value !== "" &&
+    pagesInput.value !== ""
+  ) {
+    addBookToLibrary(
+      titleInput.value,
+      authorInput.value,
+      pagesInput.value,
+      readInput.value
+    );
+    displayBooks(myLibrary);
+  } else alert("gowno");
 });
 
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  console.log(myLibrary);
+}
+
+function displayBooks(myLibrary) {
+  libraryDiv.innerHTML = "";
+
+  myLibrary.forEach((i) => {
+    let pElement = document.createElement("p");
+    for (const [key, value] of Object.entries(i)) {
+      let arrVal = document.createTextNode(`${key}: ${value}\n`);
+      pElement.appendChild(arrVal);
+      pElement.appendChild(document.createElement("br"));
+      libraryDiv.appendChild(pElement);
+    }
+  });
 }
